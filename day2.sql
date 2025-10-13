66fee23c99d399169d7d9846b85d2211210481e7
sarathhhhhh <sarathsastham@gmail.com>
2025-10-13 12:08:06 +0530

    first commit

---
+++ day2.sql
@@ -0,0 +1,4 @@
+SELECT * FROM `students` WHERE age>20;
+SELECT * FROM `students` WHERE department='Computer science' OR department='Physics';
+SELECT * FROM `students` WHERE grade="90";
+SELECT * FROM `students` WHERE grade>70 AND grade<90;
